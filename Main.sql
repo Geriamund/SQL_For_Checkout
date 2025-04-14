ec60f3b598fda7c466580710a7a3ffb51f3794c9
Geriamund <geriamund@gmail.com>
2025-04-14 15:49:52 -0400

    CREATE TABLE Pedidos Criar Table Pedidos e inserir 3 pedidos

--- Main.sql
+++ Main.sql
@@ -14,3 +14,16 @@ ALTER TABLE Produtos ADD COLUMN estoque INTEGER DEFAULT 0;
 UPDATE Produtos SET estoque = 15 WHERE id = 1;
 UPDATE Produtos SET estoque = 20 WHERE id = 2;
 UPDATE Produtos SET estoque = 10 WHERE id = 3;
+
+CREATE TABLE Pedidos (
+    id INTEGER PRIMARY KEY,
+    produto_id INTEGER NOT NULL,
+    quantidade INTEGER NOT NULL,
+    data_pedido TEXT NOT NULL, -- formato ISO 'YYYY-MM-DD'
+    FOREIGN KEY (produto_id) REFERENCES Produtos(id)
+);
+
+INSERT INTO Pedidos (id, produto_id, quantidade, data_pedido) VALUES
+(1, 1, 2, '2025-04-10'),
+(2, 2, 1, '2025-04-11'),
+(3, 3, 3, '2025-04-12');
