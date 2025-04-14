4c842357b076d9290199e8ea99cdb4ef2e42c51f
Geriamund <geriamund@gmail.com>
2025-04-14 15:47:32 -0400

    ADD COLUMN estoque Adicionar a coluna estoque na table produtos

--- Main.sql
+++ Main.sql
@@ -9,3 +9,8 @@ INSERT INTO Produtos (id, nome, preco) VALUES
 (2, 'Eragon', 39.50),
 (3, 'Harry Potter', 37.42);
 
+ALTER TABLE Produtos ADD COLUMN estoque INTEGER DEFAULT 0;
+
+UPDATE Produtos SET estoque = 15 WHERE id = 1;
+UPDATE Produtos SET estoque = 20 WHERE id = 2;
+UPDATE Produtos SET estoque = 10 WHERE id = 3;
